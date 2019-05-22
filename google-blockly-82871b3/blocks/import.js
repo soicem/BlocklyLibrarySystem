79a954df8c6a61593b9d4cf904d8a3a607028bfe
Chang-Sub Kim <peurocs4@gmail.com>
2019-05-23 02:19:35 +0900

    Added javascript generator for import blocks
    - added js code generating for import blocks at "generators/import.js"
    - added a button to replace the textarea with test xml for import blocks
    - added a button to append import blocks to toolbox dynamically (not implemented) (+1 squashed commits)
    
    Squashed commits:
    
    [cf7c338] Fixed error + removed inline status stored with mutation
    
    - fixed the problem of argsCount_ not storing correctly
    - removed inline status being stored with mutation status

--- google-blockly-82871b3/blocks/import.js
+++ google-blockly-82871b3/blocks/import.js
@@ -89,11 +89,8 @@ Blockly.Blocks['import_return'] = {
     let container = document.createElement("mutation");
 
     container.setAttribute("func", this.funcName_);
-    if (!this.argsCount_ > 0) {
+    if (this.argsCount_ > 0) {
       container.setAttribute("args", this.argsCount_);
-    }
-    if (!this.getInputsInline()) {
-      container.setAttribute("inline", this.getInputsInline());
     }
 
     for (let i = 0; i < this.argsCount_; i++) {
@@ -114,10 +111,7 @@ Blockly.Blocks['import_return'] = {
       this.argsName_[i] = (xmlElement.getElementsByTagName("arg")[i].getAttribute("name"));
     }
 
-    let inlineAttribute = xmlElement.getAttribute("inline");
-    let defaultInline = this.getInputsInline();
-    let inline = (inlineAttribute && inlineAttribute.toLowerCase() == "true") || defaultInline;
-    this.updateShape_(inline);
+    this.updateShape_();
   },
 
   decompose: function(workspace) {
@@ -134,10 +128,9 @@ Blockly.Blocks['import_return'] = {
     this.setInputsInline(inline);
   },
 
-  updateShape_: function(inline) {
+  updateShape_: function() {
     this.resetShape_();
 
-    this.setInputsInline(inline);
     this.getField("FUNC_FIELD").setText(this.funcName_);
 
     if (this.argsCount_ > 0) {
@@ -166,119 +159,157 @@ Blockly.Blocks['import_return'] = {
   }
 };
 
+Blockly.Blocks['import_statement'] = {
+  argsCount_: 0,
+  argsName_: [],
+  funcName_: '',
+
+  init: function() {
+    this.appendDummyInput("FUNC")
+        .appendField("", "FUNC_FIELD");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+    this.setTooltip("");
+    this.setHelpUrl("");
+    this.setMutator(new Blockly.Mutator([null]));
+
+    this.argsCount_ = 0;
+    this.argsName_ = [];
+    this.funcName_ = '';
+
+    this.resetShape_();
+  },
+
+  mutationToDom: Blockly.Blocks["import_return"].mutationToDom,
+
+  domToMutation: Blockly.Blocks["import_return"].domToMutation,
+
+  decompose: Blockly.Blocks["import_return"].decompose,
+
+  compose: Blockly.Blocks["import_return"].compose,
+
+  updateShape_: Blockly.Blocks["import_return"].updateShape_,
+
+  resetShape_: Blockly.Blocks["import_return"].resetShape_
+};
+
+
+
 // Blockly.Constants.Text.IMPORT_RETURN_MUTATOR_MIXIN = {
-  // /**
-  //  * Create XML to represent number of text inputs.
-  //  * @return {!Element} XML storage element.
-  //  * @this Blockly.Block
-  //  */
-  // mutationToDom: function() {
-  //   var container = document.createElement('mutation');
-  //   container.setAttribute('args', this.argsCount_);
-  //
-  //   //var argsContainer = document.createElement('')
-  //
-  //   return container;
-  // },
-  // /**
-  //  * Parse XML to restore the text inputs.
-  //  * @param {!Element} xmlElement XML storage element.
-  //  * @this Blockly.Block
-  //  */
-  // domToMutation: function(xmlElement) {
-  //   this.argsCount_ = parseInt(xmlElement.getAttribute('args'), 10);
-  //   //this.updateShape_();
-  // },
-  // /**
-  //  * Populate the mutator's dialog with this block's components.
-  //  * @param {!Blockly.Workspace} workspace Mutator's workspace.
-  //  * @return {!Blockly.Block} Root block in mutator.
-  //  * @this Blockly.Block
-  //  */
-  // decompose: function(workspace) {
-  //   var containerBlock = workspace.newBlock('text_create_join_container');
-  //   containerBlock.initSvg();
-  //   var connection = containerBlock.getInput('STACK').connection;
-  //   for (var i = 0; i < this.argsCount_; i++) {
-  //     var itemBlock = workspace.newBlock('text_create_join_item');
-  //     itemBlock.initSvg();
-  //     connection.connect(itemBlock.previousConnection);
-  //     connection = itemBlock.nextConnection;
-  //   }
-  //   return containerBlock;
-  // },
-  // /**
-  //  * Reconfigure this block based on the mutator dialog's components.
-  //  * @param {!Blockly.Block} containerBlock Root block in mutator.
-  //  * @this Blockly.Block
-  //  */
-  // compose: function(containerBlock) {
-  //   var itemBlock = containerBlock.getInputTargetBlock('STACK');
-  //   // Count number of inputs.
-  //   var connections = [];
-  //   while (itemBlock) {
-  //     connections.push(itemBlock.valueConnection_);
-  //     itemBlock = itemBlock.nextConnection &&
-  //         itemBlock.nextConnection.targetBlock();
-  //   }
-  //   // Disconnect any children that don't belong.
-  //   for (var i = 0; i < this.argsCount_; i++) {
-  //     var connection = this.getInput('ADD' + i).connection.targetConnection;
-  //     if (connection && connections.indexOf(connection) == -1) {
-  //       connection.disconnect();
-  //     }
-  //   }
-  //   this.argsCount_ = connections.length;
-  //   //this.updateShape_();
-  //   // Reconnect any child blocks.
-  //   for (var i = 0; i < this.argsCount_; i++) {
-  //     Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
-  //   }
-  // },
-  // /**
-  //  * Store pointers to any connected child blocks.
-  //  * @param {!Blockly.Block} containerBlock Root block in mutator.
-  //  * @this Blockly.Block
-  //  */
-  // saveConnections: function(containerBlock) {
-  //   var itemBlock = containerBlock.getInputTargetBlock('STACK');
-  //   var i = 0;
-  //   while (itemBlock) {
-  //     var input = this.getInput('ADD' + i);
-  //     itemBlock.valueConnection_ = input && input.connection.targetConnection;
-  //     i++;
-  //     itemBlock = itemBlock.nextConnection &&
-  //         itemBlock.nextConnection.targetBlock();
-  //   }
-  // },
-  // /**
-  //  * Modify this block to have the correct number of inputs.
-  //  * @private
-  //  * @this Blockly.Block
-  //  */
-  // updateShape_: function() {
-  //   if (this.argsCount_ && this.getInput('EMPTY')) {
-  //     this.removeInput('EMPTY');
-  //   } else if (!this.argsCount_ && !this.getInput('EMPTY')) {
-  //     this.appendDummyInput('EMPTY')
-  //         .appendField(this.newQuote_(true))
-  //         .appendField(this.newQuote_(false));
-  //   }
-  //   // Add new inputs.
-  //   for (var i = 0; i < this.argsCount_; i++) {
-  //     if (!this.getInput('ADD' + i)) {
-  //       var input = this.appendValueInput('ADD' + i);
-  //       if (i == 0) {
-  //         input.appendField(Blockly.Msg['TEXT_JOIN_TITLE_CREATEWITH']);
-  //       }
-  //     }
-  //   }
-  //   // Remove deleted inputs.
-  //   while (this.getInput('ADD' + i)) {
-  //     this.removeInput('ADD' + i);
-  //     i++;
-  //   }
-  // }
+// /**
+//  * Create XML to represent number of text inputs.
+//  * @return {!Element} XML storage element.
+//  * @this Blockly.Block
+//  */
+// mutationToDom: function() {
+//   var container = document.createElement('mutation');
+//   container.setAttribute('args', this.argsCount_);
+//
+//   //var argsContainer = document.createElement('')
+//
+//   return container;
+// },
+// /**
+//  * Parse XML to restore the text inputs.
+//  * @param {!Element} xmlElement XML storage element.
+//  * @this Blockly.Block
+//  */
+// domToMutation: function(xmlElement) {
+//   this.argsCount_ = parseInt(xmlElement.getAttribute('args'), 10);
+//   //this.updateShape_();
+// },
+// /**
+//  * Populate the mutator's dialog with this block's components.
+//  * @param {!Blockly.Workspace} workspace Mutator's workspace.
+//  * @return {!Blockly.Block} Root block in mutator.
+//  * @this Blockly.Block
+//  */
+// decompose: function(workspace) {
+//   var containerBlock = workspace.newBlock('text_create_join_container');
+//   containerBlock.initSvg();
+//   var connection = containerBlock.getInput('STACK').connection;
+//   for (var i = 0; i < this.argsCount_; i++) {
+//     var itemBlock = workspace.newBlock('text_create_join_item');
+//     itemBlock.initSvg();
+//     connection.connect(itemBlock.previousConnection);
+//     connection = itemBlock.nextConnection;
+//   }
+//   return containerBlock;
+// },
+// /**
+//  * Reconfigure this block based on the mutator dialog's components.
+//  * @param {!Blockly.Block} containerBlock Root block in mutator.
+//  * @this Blockly.Block
+//  */
+// compose: function(containerBlock) {
+//   var itemBlock = containerBlock.getInputTargetBlock('STACK');
+//   // Count number of inputs.
+//   var connections = [];
+//   while (itemBlock) {
+//     connections.push(itemBlock.valueConnection_);
+//     itemBlock = itemBlock.nextConnection &&
+//         itemBlock.nextConnection.targetBlock();
+//   }
+//   // Disconnect any children that don't belong.
+//   for (var i = 0; i < this.argsCount_; i++) {
+//     var connection = this.getInput('ADD' + i).connection.targetConnection;
+//     if (connection && connections.indexOf(connection) == -1) {
+//       connection.disconnect();
+//     }
+//   }
+//   this.argsCount_ = connections.length;
+//   //this.updateShape_();
+//   // Reconnect any child blocks.
+//   for (var i = 0; i < this.argsCount_; i++) {
+//     Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
+//   }
+// },
+// /**
+//  * Store pointers to any connected child blocks.
+//  * @param {!Blockly.Block} containerBlock Root block in mutator.
+//  * @this Blockly.Block
+//  */
+// saveConnections: function(containerBlock) {
+//   var itemBlock = containerBlock.getInputTargetBlock('STACK');
+//   var i = 0;
+//   while (itemBlock) {
+//     var input = this.getInput('ADD' + i);
+//     itemBlock.valueConnection_ = input && input.connection.targetConnection;
+//     i++;
+//     itemBlock = itemBlock.nextConnection &&
+//         itemBlock.nextConnection.targetBlock();
+//   }
+// },
+// /**
+//  * Modify this block to have the correct number of inputs.
+//  * @private
+//  * @this Blockly.Block
+//  */
+// updateShape_: function() {
+//   if (this.argsCount_ && this.getInput('EMPTY')) {
+//     this.removeInput('EMPTY');
+//   } else if (!this.argsCount_ && !this.getInput('EMPTY')) {
+//     this.appendDummyInput('EMPTY')
+//         .appendField(this.newQuote_(true))
+//         .appendField(this.newQuote_(false));
+//   }
+//   // Add new inputs.
+//   for (var i = 0; i < this.argsCount_; i++) {
+//     if (!this.getInput('ADD' + i)) {
+//       var input = this.appendValueInput('ADD' + i);
+//       if (i == 0) {
+//         input.appendField(Blockly.Msg['TEXT_JOIN_TITLE_CREATEWITH']);
+//       }
+//     }
+//   }
+//   // Remove deleted inputs.
+//   while (this.getInput('ADD' + i)) {
+//     this.removeInput('ADD' + i);
+//     i++;
+//   }
+// }
 // };
 
 // /**
---
+++ google-blockly-82871b3/generators/javascript/import.js
@@ -0,0 +1,25 @@
+Blockly.JavaScript['import_return'] = function(block) {
+    let funcName = block.getFieldValue("FUNC_FIELD").replace(" ", "_");
+    let args = [];
+
+    for (let i = 0; i < block.argsCount_; i++) {
+        let noConnectionValue = "null";
+        args[i] = Blockly.JavaScript.valueToCode(block, "ARG" + i, Blockly.JavaScript.ORDER_ATOMIC) || noConnectionValue;
+    }
+
+    let code = funcName + "(" + args.join(", ") + ")";
+    return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+}
+
+Blockly.JavaScript['import_statement'] = function(block) {
+    let funcName = block.getFieldValue("FUNC_FIELD").replace(" ", "_");
+    let args = [];
+
+    for (let i = 0; i < block.argsCount_; i++) {
+        let noConnectionValue = "null";
+        args[i] = Blockly.JavaScript.valueToCode(block, "ARG" + i, Blockly.JavaScript.ORDER_ATOMIC) || noConnectionValue;
+    }
+
+    let code = funcName + "(" + args.join(", ") + ");\n";
+    return code;
+}
