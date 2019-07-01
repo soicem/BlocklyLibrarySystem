49f265a8ef53b1f4238fd349503d78260096bad7
Chang-Sub Kim <peurocs4@gmail.com>
2019-07-01 18:47:50 +0900

    Added all "controls" blocks

--- google-blockly-82871b3/blocks/sprites/controls.js
+++ google-blockly-82871b3/blocks/sprites/controls.js
@@ -30,16 +30,162 @@ goog.require('Blockly.Blocks');
 goog.require('Blockly');
 
 
-Blockly.Blocks['sprite_forever'] = {
+Blockly.Blocks['sprite_wait'] = {
   init: function() {
+    this.appendValueInput("SEC")
+    .setCheck(null)
+    .appendField("wait");
     this.appendDummyInput()
-    .appendField("forever");
-    this.appendStatementInput("STATEMENTS")
-    .setCheck(null);
+    .appendField("seconds");
+    this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(230);
+    this.setColour(40);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
+
+Blockly.Blocks['sprite_repeat'] = {
+  init: function() {
+    this.appendValueInput("VALUE")
+    .setCheck(null)
+    .appendField("repeat");
+    this.appendStatementInput("STATEMENT")
+    .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_forever'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("forever");
+    this.appendStatementInput("STATEMENT")
+    .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_if'] = {
+  init: function() {
+    this.appendValueInput("COND")
+    .setCheck(null)
+    .appendField("if");
+    this.appendDummyInput()
+    .appendField("then");
+    this.appendStatementInput("STATEMENT")
+    .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_ifElse'] = {
+  init: function() {
+    this.appendValueInput("COND")
+    .setCheck(null)
+    .appendField("if");
+    this.appendDummyInput()
+    .appendField("then");
+    this.appendStatementInput("STATEMENT")
+    .setCheck(null);
+    this.appendDummyInput()
+    .appendField("else");
+    this.appendStatementInput("ELSE_STATEMENT")
+    .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_waitTill'] = {
+  init: function() {
+    this.appendValueInput("COND")
+    .setCheck(null)
+    .appendField("wait until");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_repeatTill'] = {
+  init: function() {
+    this.appendValueInput("COND")
+    .setCheck(null)
+    .appendField("repeat until");
+    this.appendStatementInput("STATEMENT")
+    .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_stop'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("stop")
+    .appendField(new Blockly.FieldDropdown([["all","ALL"], ["this script","THIS"], ["other scripts in sprite","OTHER"]]), "OPTION");
+    this.setPreviousStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_cloneStarted'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("when I start as a clone");
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_createClone'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("create clone of")
+    .appendField(new Blockly.FieldDropdown([["myself","ME"]]), "OPTION");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_deleteClone'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("delete this clone");
+    this.setPreviousStatement(true, null);
+    this.setColour(40);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
