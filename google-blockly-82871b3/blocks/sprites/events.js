b0d115542c52ba6083baa2156770d9507776cb59
peurocs4 <peurocs4@gmail.com>
2019-07-02 15:44:39 +0900

    Csk sprite (#9)
    * Added all "events" blocks
    
    * Added all "controls" blocks
    
    * Added all "sensing" blocks
    
    * Added all "operators" blocks
    
    * Fixed some bugs
    
    - "think for sec" block not showing default "sec" block
    - "get mouse X/Y" block not showing both "x" and "y" block separately

--- google-blockly-82871b3/blocks/sprites/events.js
+++ google-blockly-82871b3/blocks/sprites/events.js
@@ -38,8 +38,172 @@ Blockly.Blocks['sprite_flagClicked'] = {
     .appendField("clicked");
     this.setInputsInline(true);
     this.setNextStatement(true, null);
-    this.setColour(230);
+    this.setColour(60);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
+
+Blockly.Blocks['sprite_keyPressed'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("when")
+    .appendField(new Blockly.FieldDropdown([
+        ["any","ANY"],
+        ["space","SPACE"],
+        ["up arrow","UP"],
+        ["down arrow","DOWN"],
+        ["right arrow","RIGHT"],
+        ["left arrow","LEFT"],
+        ["a","A"],
+        ["b","B"],
+        ["c","C"],
+        ["d","D"],
+        ["e","E"],
+        ["f","F"],
+        ["g","G"],
+        ["h","H"],
+        ["i","I"],
+        ["j","J"],
+        ["k","K"],
+        ["l","L"],
+        ["m","M"],
+        ["n","N"],
+        ["o","O"],
+        ["p","P"],
+        ["q","Q"],
+        ["r","R"],
+        ["s","S"],
+        ["t","T"],
+        ["u","U"],
+        ["v","V"],
+        ["w","W"],
+        ["x","X"],
+        ["y","Y"],
+        ["z","Z"],
+        ["0","ZERO"],
+        ["1","ONE"],
+        ["2","TWO"],
+        ["3","THREE"],
+        ["4","FOUR"],
+        ["5","FIVE"],
+        ["6","SIX"],
+        ["7","SEVEN"],
+        ["8","EIGHT"],
+        ["9","NINE"]
+    ]), "KEY")
+    .appendField("key pressed");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_spriteClicked'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("when this sprite clicked");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_backdropSwitched'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("when backdrop switches to")
+    .appendField(new Blockly.FieldDropdown([["backdrop1","BACKDROP1"]]), "BACKDROP");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_loudnessCondition'] = {
+  init: function() {
+    this.appendValueInput("VALUE")
+    .setCheck(null)
+    .appendField("when loudness")
+    .appendField(new Blockly.FieldDropdown([[">","GTR"], [">=","GTE"], ["=","EQU"], ["<=","LSE"], ["<","LSS"]]), "COND");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_timerCondition'] = {
+  init: function() {
+    this.appendValueInput("VALUE")
+    .setCheck(null)
+    .appendField("when timer")
+    .appendField(new Blockly.FieldDropdown([[">","GTR"], [">=","GTE"], ["=","EQU"], ["<=","LSE"], ["<","LSS"]]), "COND");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_messageReceived'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("when I receive")
+    .appendField(new Blockly.FieldDropdown([["message1","MESSAGE1"]]), "MESSAGE");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_messageBroadcast'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("broadcast")
+    .appendField(new Blockly.FieldDropdown([["message1","MESSAGE1"]]), "MESSAGE");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_messageBroadcastWait'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("broadcast")
+    .appendField(new Blockly.FieldDropdown([["message1","MESSAGE1"]]), "MESSAGE")
+    .appendField("wait");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
--- google-blockly-82871b3/blocks/sprites/sensing.js
+++ google-blockly-82871b3/blocks/sprites/sensing.js
@@ -30,27 +30,266 @@ goog.require('Blockly.Blocks');
 goog.require('Blockly');
 
 
-Blockly.Blocks['sprite_senseTouch'] = {
+Blockly.Blocks['sprite_touching'] = {
   init: function() {
-    this.appendValueInput("SPRITE")
-    .setCheck(null)
-    .appendField("touching");
+    this.appendDummyInput()
+    .appendField("touching")
+    .appendField(new Blockly.FieldDropdown([["mouse pointer","MOUSE"], ["edge","EDGE"]]), "OPTION");
     this.setInputsInline(true);
     this.setOutput(true, null);
-    this.setColour(230);
+    this.setColour(190);
     this.setTooltip("");
     this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['sprite_senseColor'] = {
+Blockly.Blocks['sprite_touchingColor'] = {
   init: function() {
     this.appendDummyInput()
     .appendField("touching color")
     .appendField(new Blockly.FieldColour("#0000ff"), "COLOR");
     this.setInputsInline(true);
     this.setOutput(true, null);
-    this.setColour(230);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_colorTouchingColor'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField(new Blockly.FieldColour("#0000ff"), "COLOR1")
+    .appendField("is touching")
+    .appendField(new Blockly.FieldColour("#ff0000"), "COLOR2");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_distanceTo'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("distance to")
+    .appendField(new Blockly.FieldDropdown([["mouse pointer","MOUSE"]]), "OPTION");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_askWait'] = {
+  init: function() {
+    this.appendValueInput("MESSAGE")
+    .setCheck(null)
+    .appendField("ask");
+    this.appendDummyInput()
+    .appendField("and wait");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_answer'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("answer");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_keyPress'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("key")
+    .appendField(new Blockly.FieldDropdown([
+      ["any","ANY"],
+      ["space","SPACE"],
+      ["up arrow","UP"],
+      ["down arrow","DOWN"],
+      ["right arrow","RIGHT"],
+      ["left arrow","LEFT"],
+      ["a","A"],
+      ["b","B"],
+      ["c","C"],
+      ["d","D"],
+      ["e","E"],
+      ["f","F"],
+      ["g","G"],
+      ["h","H"],
+      ["i","I"],
+      ["j","J"],
+      ["k","K"],
+      ["l","L"],
+      ["m","M"],
+      ["n","N"],
+      ["o","O"],
+      ["p","P"],
+      ["q","Q"],
+      ["r","R"],
+      ["s","S"],
+      ["t","T"],
+      ["u","U"],
+      ["v","V"],
+      ["w","W"],
+      ["x","X"],
+      ["y","Y"],
+      ["z","Z"],
+      ["0","ZERO"],
+      ["1","ONE"],
+      ["2","TWO"],
+      ["3","THREE"],
+      ["4","FOUR"],
+      ["5","FIVE"],
+      ["6","SIX"],
+      ["7","SEVEN"],
+      ["8","EIGHT"],
+      ["9","NINE"]
+    ]), "KEY")
+    .appendField("pressed?");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_mouseDown'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("mouse down?");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getMouseXY'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("mouse")
+    .appendField(new Blockly.FieldDropdown([["x","X"], ["y","Y"]]), "XY");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_setDragMode'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("set drag mode:")
+    .appendField(new Blockly.FieldDropdown([["draggable","DRAGGABLE"], ["not draggable","NOT_DRAGGABLE"]]), "OPTION");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getLoudness'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("loudness");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getTimer'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("timer");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_resetTimer'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("reset timer");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getStage'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField(new Blockly.FieldDropdown([["backdrop #","BACKDROP_NUM"], ["backdrop name","BACKDROP_NAME"], ["volume","VOLUME"], ["my variable","VARIABLE"]]), "SRC")
+    .appendField("of")
+    .appendField(new Blockly.FieldDropdown([["stage","STAGE"]]), "OPTION");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getDate'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("current")
+    .appendField(new Blockly.FieldDropdown([["date","DATE"], ["year","YEAR"], ["month","MONTH"], ["day of week","DAY"], ["hour","HOUR"], ["minute","MINUTE"], ["second","SECOND"]]), "OPTION");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getSince'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField(new Blockly.FieldDropdown([["years","YEAR"], ["months","MONTH"], ["days","DAY"]]), "OPTION")
+    .appendField("since 2000");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sprite_getUsername'] = {
+  init: function() {
+    this.appendDummyInput()
+    .appendField("username");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(190);
     this.setTooltip("");
     this.setHelpUrl("");
   }
