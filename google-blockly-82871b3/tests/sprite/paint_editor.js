e6c8affe9718d70598f2027228ed862bd3b37313
myulchkun <steven800@naver.com>
2019-07-10 02:32:19 +0900

    UI수정
    UI수정

--- google-blockly-82871b3/tests/sprite/paint_editor.js
+++ google-blockly-82871b3/tests/sprite/paint_editor.js
@@ -60,3 +60,9 @@ function getPosition(event) {
     var y = event.pageY - canvas.offsetTop;
     return {X: x, Y: y};
 }
+
+function convertCanvasToImage(canvas){
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image;
+}
