76ba7fdf9522fd5fcd2fa95548a0f46dd3b2d29c
Chang-Sub Kim <peurocs4@gmail.com>
2019-07-25 05:48:18 +0900

    Added translation (not 100%)

---
+++ google-blockly-82871b3/tests/src/translate/en.js
@@ -0,0 +1,17 @@
+Blockly.Msg.SPRITE_RANDOM_POSITION = "random position";
+Blockly.Msg.SPRITE_MOUSE_POSITION = "mouse position";
+
+
+Blockly.Msg.SPRITE_MOVESTEPS_MSG = "move %1 steps";
+Blockly.Msg.SPRITE_TURNANGLE_MSG = "turn %1 %2";
+Blockly.Msg.SPRITE_GOTO_MSG = "go to %1";
+Blockly.Msg.SPRITE_GOTOPOINT_MSG_PREFIX = "go to";
+Blockly.Msg.SPRITE_GOTOPOINT_MSG = "x: %1 y: %2";
+Blockly.Msg.SPRITE_GLIDETO_MSG = "glide %1 secs to %2";
+Blockly.Msg.SPRITE_GLIDETOPOINT_MSG = "glide %1 secs to x: %2 y: %3";
+Blockly.Msg.SPRITE_POINTTO_MSG = "point in direction %1";
+Blockly.Msg.SPRITE_POINTTOWARD_MSG = "point towards %1";
+Blockly.Msg.SPRITE_CHANGEXY_MSG = "change %1 by %2";
+Blockly.Msg.SPRITE_SETXY_MSG = "set %1 to %2";
+Blockly.Msg.SPRITE_GETXY_MSG = "%1 position";
+Blockly.Msg.SPRITE_GETDIR_MSG = "direction";
---
+++ google-blockly-82871b3/tests/src/translate/ko.js
@@ -0,0 +1,17 @@
+Blockly.Msg.SPRITE_RANDOM_POSITION = "무작위 위치";
+Blockly.Msg.SPRITE_MOUSE_POSITION = "마우스 위치";
+
+
+Blockly.Msg.SPRITE_MOVESTEPS_MSG = "%1만큼 움직이기";
+Blockly.Msg.SPRITE_TURNANGLE_MSG = "%2도 만큼 %1로 돌리기";
+Blockly.Msg.SPRITE_GOTO_MSG = "%1로 가기";
+Blockly.Msg.SPRITE_GOTOPOINT_MSG_PREFIX = "";
+Blockly.Msg.SPRITE_GOTOPOINT_MSG = "x는 %1 y는 %2로 변경";
+Blockly.Msg.SPRITE_GLIDETO_MSG = "%2로 %1초동안 움직이기";
+Blockly.Msg.SPRITE_GLIDETOPOINT_MSG = "x는 %2 y는 %3로 %1초동안 움직이기";
+Blockly.Msg.SPRITE_POINTTO_MSG = "%1도 방향으로 돌아보기";
+Blockly.Msg.SPRITE_POINTTOWARD_MSG = "%1 방향으로 돌아보기";
+Blockly.Msg.SPRITE_CHANGEXY_MSG = "%1를 %2 값만큼 변경";
+Blockly.Msg.SPRITE_SETXY_MSG = "%1를 %2 값으로 변경";
+Blockly.Msg.SPRITE_GETXY_MSG = "%1 위치";
+Blockly.Msg.SPRITE_GETDIR_MSG = "방향";
