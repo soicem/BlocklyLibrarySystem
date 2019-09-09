6fe0c9f6261ab5cfa191fa53f7169562b032e5b5
Chang-Sub Kim <peurocs4@gmail.com>
2019-09-09 10:07:56 +0900

    Added jsDoc for my exceptions

--- google-blockly-82871b3/tests/src/js/exception/MyException.js
+++ google-blockly-82871b3/tests/src/js/exception/MyException.js
@@ -1,43 +1,75 @@
 class MyException {
+  /**
+   * @param {string} exceptionName
+   * @param {string} message
+   */
   constructor(exceptionName, message) {
     this.name = exceptionName;
     this.message = message;
   }
 
+  /**
+   * @returns {string}
+   */
   get message() {
     return this._message;
   }
 
-  set message(value) {
-    this._message = value;
+  /**
+   * @param {string} message
+   */
+  set message(message) {
+    this._message = message;
   }
 
+  /**
+   * @returns {string}
+   */
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = value;
+  /**
+   * @param {string} name
+   */
+  set name(name) {
+    this._name = name;
   }
 
+  /**
+   * Returns exception in a string format
+   * @returns {string}
+   */
   toString() {
     return this._name + ": " + this._message;
   }
 }
 
+
 class IllegalArgumentException extends MyException {
+  /**
+   * @param {string} message
+   */
   constructor(message) {
     super("IllegalArgumentException", message);
   }
 }
 
+
 class AbstractClassConstructException extends MyException {
+  /**
+   * @param {string} message
+   */
   constructor(message) {
     super("AbstractClassConstructException", message);
   }
 }
 
+
 class AbstractMethodCallException extends  MyException {
+  /**
+   * @param {string} message
+   */
   constructor(message) {
     super("AbstractMethodCallException", message);
   }
